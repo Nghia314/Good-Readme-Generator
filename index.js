7017ba25a78a5c3be6396ca2567b4be6ca518e3c
nghia le <calvinle12350@gmail.com>
2022-04-20 18:06:58 -0700

    refector the error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const fileName = "README.md";
 const util= require('util');
 
